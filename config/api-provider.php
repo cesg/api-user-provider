979e2057ec385d0b2d897b9797cf8c8b955827fb
cristianSG <cesgbla@gmail.com>
2018-06-12 16:00:07 -0400

    feat(config) Add TTL in cache.

--- config/api-provider.php
+++ config/api-provider.php
@@ -3,7 +3,7 @@
 return [
     'cache-key' => 'user-api-access-token',
     'uri' => env('USER_PROVIDER_API_URL', 'localhost/api/v1/users'),
-    'cache_ttl' => 2,
+    'cache_ttl' => 10,
     'headers' => [
     ],
 ];
